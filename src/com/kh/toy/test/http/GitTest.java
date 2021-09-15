46b6f85cff7985e1306f29ea840fe135e43636e4
lhj132824 <88864144+lhj132824@users.noreply.github.com>
2021-09-15 14:52:22 +0900

    Update GitTest.java
    [원격리포지토리] 테스트

--- src/com/kh/toy/test/http/GitTest.java
+++ src/com/kh/toy/test/http/GitTest.java
@@ -3,4 +3,5 @@ package com.kh.toy.test.http;
 public class GitTest {
 	private static final String DESC = "GIT TEST용 클래스 입니다.";
 	private static final String CREDENTIAL = "GIT 인증 테스트";
+	private static final String GITHUB = "원격리포지토리에서 직접 수정";
 }
